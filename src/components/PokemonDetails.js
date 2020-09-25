a408be2b6bab370b680c218f48e29c820886e29f
Takács Veronika <roni9764@gmail.com>
2020-09-26 00:05:02 +0200

    Rewrite PockemonDetails.js to use functional components

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -1,39 +1,41 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Abilities from "./pokemondetails/Abilities";
 
-export class PokemonDetails extends Component {
-  state = {
-    abilities: [],
-    picture: "",
-    name: "",
-    pokemonId: ""
-  };
+const PokemonDetails = props => {
+  // state = {
+  //   abilities: [],
+  //   picture: "",
+  //   name: "",
+  //   pokemonId: ""
+  // };
+  const [abilities, setAbilities] = useState([]);
+  const [picture, setPicture] = useState("");
+  const [name, setName] = useState("");
+  const [pokemonId, setPokemonId] = useState("");
 
-  componentDidMount() {
+  useEffect(() => {
     let path = window.location.pathname;
     let id = path.split("/")[path.split("/").length - 1];
-    this.setState({pokemonId: id})
+    setPokemonId(id);
     let link = `https://pokeapi.co/api/v2/pokemon/${id}/`;
-    axios.get(link).then((response) =>
-      this.setState({
-        abilities: response.data.abilities,
-        picture: response.data.sprites.other["official-artwork"].front_default,
-        name: response.data.forms[0].name,
-      })
+    axios.get(link).then((response) => {
+      setAbilities(response.data.abilities);
+      setPicture(response.data.sprites.other["official-artwork"].front_default);
+      setName(response.data.forms[0].name)
+    });
+  }, []) 
+    
+
+  return (
+    <div>
+      <img src={picture} alt="pokemon" />
+      <p>Name: {name}</p>
+      <p>Abilities:</p>
+      <Abilities abilities={abilities} id={pokemonId} />
+    </div>
     );
   }
 
-  render() {
-    return (
-      <div>
-        <img src={this.state.picture} alt="pokemon" />
-    <p>Name: {this.state.name}</p>
-    <p>Abilities:</p>
-    <Abilities abilities={this.state.abilities} id={this.state.pokemonId} />
-      </div>
-    );
-  }
-}
 
 export default PokemonDetails;
