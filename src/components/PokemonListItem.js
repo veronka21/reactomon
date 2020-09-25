7e09e51ad74cd8676dd47003a876c8d640bd3f1d
Takács Veronika <roni9764@gmail.com>
2020-09-25 23:53:16 +0200

    Rewrite PokemonListItem.js to use functional components

--- src/components/PokemonListItem.js
+++ src/components/PokemonListItem.js
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from 'react-router-dom';
 
-export class PokemonListItem extends Component {
-  render() {
-    let url = this.props.pokemon.url;
+const PokemonListItem = props => {
+    let url = props.pokemon.url;
     let splitted = url.split("/");
     let id = splitted[splitted.length - 2];
     let link = `/pokemon/${id}`;
     return (
       <li>
-        <Link to={link}>{this.props.pokemon.name}</Link>
+        <Link to={link}>{props.pokemon.name}</Link>
       </li>
     );
   }
-}
 
 export default PokemonListItem;
