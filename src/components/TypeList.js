af3e3aa83859d67960ee3b1ddf5bdb1be5cc4349
Takács Veronika <roni9764@gmail.com>
2020-09-25 23:55:24 +0200

    Rewrite TypeList.js to use functional components

--- src/components/TypeList.js
+++ src/components/TypeList.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class Types extends Component {
-    render() {
-        return (
-            this.props.types.map(type => (
-            <li key={type.url}>{type.name}</li>
-            ))
-        )
-    }
+const Types = props => {
+    return (
+        props.types.map(type => (
+        <li key={type.url}>{type.name}</li>
+        ))
+    )
 }
 
 export default Types
